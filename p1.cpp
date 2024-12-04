6c6ea4ad20a97f1070c232da057f7d6d32c1e0bf
AfonsoMenconcaRibeiro <afonso.mendonca.ribeiro@tecnico.ulisboa.pt>
2024-12-04 23:31:19 +0000

    alteração de scanf's para cin/cout; pode ter alterações mal feitas nomeadamente ponteiros ATENCAO

--- p1.cpp
+++ p1.cpp
@@ -5,71 +5,49 @@
 
 using namespace std;
 
-bool equationOp(int y, int x, vector<vector<int>> table, int tsize) {
-    for (int i = 1; i <= tsize; i++) {
-        if (table[i][x] == y) {
-            return true;
+int n, m, goal;
+vector<vector<int>> *table;
+list<int> *sequence; 
+
+void storeData();
+int goalCheck(vector<vector<int>> *table, int n, int j, int goal, vector<int> *iToCheck);
+
+int main () {}
+
+void storeData() {
+    cin >> n >> m;
+    table = new vector<vector<int>>(n, vector<int>(n));      //eu tentei colocar *table = new vector<vector<int>>(n, vector<int>(n)); mas não deu certo
+    sequence = new list<int>(m);                            //eu tentei colocar *sequence = new list<int>(m); mas não deu certo
+    int value, num;                                         //mas tambem nao tenho a certeza se era suposto fazer isso
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> value;  //value = (*table)[i][j];
+            table[i][j].push_back(value); //stores the value in the table
+        }
+        cin.ignore();
+    } for (int i = 0; i < m; i++) {
+        cin >> num;  //num = (*sequence)[i];
+        sequence[i].push_back(num);  //stores one of the numbers in the sequence
+
+    }
+    
+}
+
+int goalCheck(vector<vector<int>> *table, int n, int j, int goal, vector<int> *iToCheck) {
+    int samei = 0, diffi = n;
+    list<int> *iToCheck = new list<int>(n);
+    for (int i = 0; i < n; i++) {
+        if ((*table)[i][j-1] == goal) {
+            (*iToCheck).push_back(i);    //if i equals goal, it is stored in the beggining of the list          //consegui alterar isto
+        
+        //este nao sei como fazer, tentei utilizar o insert mas nao consegui desta forma: (*iToCheck).insert(n--, i);
+
+        } else {
+            (*iToCheck)[--diffi] = i;  //stores in the end i if it doesn't equal goal
         }
     }
-    return false;
+    return samei;    
 }
 
-string putParenthesis(vector<vector<int>> table, vector<int> sequence, int tsize, int ssize, int *state) {
-
-    int x = sequence[ssize];
-    string s;
-    if (*state == 2) {
-        *state = 1;
-        return "(" + putParenthesis(table, sequence, tsize, ssize, state) + ")";
-    }
-    if (equationOp(sequence[ssize + 1], x, table, tsize)) {
-        s = "(" + putParenthesis(table, sequence, tsize, ssize-1, state) + ") " + to_string(x);
-        return s;
-    } else if (ssize > 1) {
-        s = "(" + putParenthesis(table, sequence, tsize, ssize-1, state) + " " + to_string(x) + ")";
-        return  s;
-    } else if (ssize == 1) {
-        return to_string(x);
-    }
-    *state = 0;
-    return "";
-}
-
-void storeData(
-        vector<vector<int>> *table, int *n, 
-        vector<int> *sequence, int *m, 
-        int *goal
-        ) {
-    scanf("%d %d\n", n, m);
-
-
-    string result;
-    int inn, out, tsize, ssize, state;
-    scanf("%d %d\n", &tsize, &ssize);
-    vector<vector<int>> table(tsize + 1, vector<int>(tsize + 1));
-    vector<int> sequence(ssize+2);
-    for (out = 1; out <= tsize; out++)
-        for (inn = 1; inn <= tsize; inn++) 
-            scanf("%d", &table[out][inn]);
-    for (out = 1; out <= ssize + 1; out++) 
-        scanf("%d", &sequence[out]);
-}
-
-
-int main () {
-    vector<vector<int>> table;
-    vector<int> sequence;
-    int n, m, goal;
-
-    storeData(&table, &n, &sequence, &m, &goal);
-    
-
-
-    state = 2;
-    result = putParenthesis(table, sequence, tsize, ssize, &state);
-    cout << state << endl;
-    if (state == 1) cout << result[1] << endl;
-
-    return 0;
-}
 
